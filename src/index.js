12eb23bbf2ec9d6e2404a167c757d133de2f8857
RICKCOYL <rickoburu@gmail.com>
2021-01-29 17:51:07 +0300

    add change tab functionality

--- src/index.js
+++ src/index.js
@@ -1,39 +1,50 @@
-import {menu, hideMenu}from "./utils/pages/menu";
-import navbar from "./utils/pages/navbar.js";
-import {contact,hideContact} from "./utils/pages/contact";
-import {home} from "./utils/pages/home"
+import { menu } from './utils/pages/menu';
+import navbar from './utils/pages/navbar.js';
+import { contact } from './utils/pages/contact';
+import { home } from './utils/pages/home';
 
-import  "./utils/css/style.css";
+import './utils/css/style.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 const display = () => {
-    const content = document.querySelector('#content');
+  const content = document.querySelector('#content');
 
-content.innerHTML = `
+  content.innerHTML = `
 <div>
 ${navbar()}
 </div>
 <div id="display-text">
 ${home()}
-${ menu()}
-
-${contact()}
 </div>
 
 
-`
-}
+`;
+};
 
-const loadEventListners = () =>{
-  hideContact()
-  hideMenu()
-    //homebtn.addEventListener('click',loadHome);
-    menubtn.addEventListener('click',hideMenu);
-    contactbtn.addEventListener('click',hideContact);
-}
+const loadEventListners = () => {
+  const switchTab = (event) => {
+    event.preventDefault();
+    const content = document.querySelector('#display-text');
+    switch (event.target.textContent) {
+      case 'Contact':
+        content.innerHTML = contact();
+        break;
+      case 'Menu':
+        content.innerHTML = menu();
+        break;
+      default:
+        content.innerHTML = home();
+    }
 
+    const currentActive = document.querySelectorAll('.nav-links a.active');
+    currentActive.forEach((node) => node.classList.remove('active'));
+    event.target.classList.add('active');
+  };
 
-display()
-//loadEventListners()
+  const navLinks = document.querySelectorAll('.nav-links a');
+  navLinks.forEach((navLink) => navLink.addEventListener('click', switchTab));
+};
+
+display();
+loadEventListners();
